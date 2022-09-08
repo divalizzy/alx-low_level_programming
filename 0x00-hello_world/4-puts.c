d73bf6c4929a98a629360b779bfac5650b76dcfc
divalizzy <lizzyrufus19@gmail.com>
2022-09-08 12:58:53 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -9,6 +9,6 @@
  */
 int main(void)
 {
-             printf("with proper grammar,but the outcome is a piece of art,\n");
+             puts("\"programming is like building a multilingual puzzle");
              return (0);
 }
