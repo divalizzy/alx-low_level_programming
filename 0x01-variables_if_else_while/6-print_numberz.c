348930d694729662a7c9c9df251f408fee0892f5
divalizzy <lizzyrufus19@gmail.com>
2022-09-09 13:33:43 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -5,13 +5,13 @@
  * Description: using the main function
  * this program prints "programming is positive, zero, or negative
  * Return: 0
- */int main (void)
+ */int main(void)
 {
 int n;
-for  (n = '0'; n <= '9'; n++)
+for (n = '0'; n <= '9'; n++)
 {
 	putchar(n) ;
 }
-putchar ('\n') ;
-return  (0) ;
+putchar('\n') ;
+return (0) ;
 }
