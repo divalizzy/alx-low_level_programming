ee52b8e484b56bbb314126504118bed31bcbd380
divalizzy <lizzyrufus19@gmail.com>
2022-09-15 03:26:58 -0700

    0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* main -  tests function that prints if integer is positive or negqtive
+* Return: 0
+*/
+
+int main(void)
+{
+int i;
+
+i = 0;
+positive_ or_negative(i);
+
+return (0);
+}
