c0457be92f71d0e95b9ed02a32231de4aeec95f6
divalizzy <lizzyrufus19@gmail.com>
2022-09-15 19:27:13 -0700

    3-print_remaining_days.c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -20,11 +20,6 @@ day++;
 
 printf("Day of the year: %d\n", day);
 printf("Remaining days: %d\n", 366 - day);
-day++
-}
-
-printf("Day of the year: %d\n", day);
-printf("Remaining days: %d\n", 366 - day);
 }
 else
 {
