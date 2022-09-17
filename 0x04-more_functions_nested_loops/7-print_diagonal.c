3227553b1a1f204472b69eb2eec722140a450538
divalizzy <lizzyrufus19@gmail.com>
2022-09-16 22:28:03 -0700

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -18,13 +18,14 @@ void print_diagonal(int n)
 			for (space = 0; space < len; space++)
 			{
 				putchar(' ');
-		}
+			}
+
 			putchar('\\');
 
 			if (len == (n - 1))
 			{
 				continue;
-			}	
+			}
 			putchar('\n');
 		}
 	}
